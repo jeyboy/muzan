65727861e89707a48d8378b65cb8cc0b826dbbff
Jenua Boiko <jeyboy1985@gmail.com>
2024-10-13 09:51:45 +0000

    updates for configuration

--- server/utils/config.ts
+++ server/utils/config.ts
@@ -1,4 +1,6 @@
 import Dotenv from 'dotenv'
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 Dotenv.config()
 
@@ -6,6 +8,9 @@ export const {
   NODE_ENV = 'production',
   PORT = 3000
 } = process.env
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 
 export const IS_PRODUCTION = NODE_ENV === 'production'
 export const CLIENT_BUILD_PATH = `${__dirname}/../../build`
