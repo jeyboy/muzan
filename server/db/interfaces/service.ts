cd483cbfecb11ad04d0013aba3497763512de5ab
jeyboy <eboyko@glenflow.com>
2024-11-06 11:27:09 +0200

    updates for service

--- server/db/interfaces/service.ts
+++ server/db/interfaces/service.ts
@@ -1,7 +1,25 @@
 import { type Document } from "mongodb";
 
+export class ServicePreset {
+    public name: string;
+    public identifier: string;
+    public description: string | undefined;
+
+    constructor(data: (Partial<ServicePreset> & { name: string, identifier: string })) {
+        this.name = data.name;
+        this.identifier = data.identifier;
+        this.description = data.description;
+    }
+}
+
 export class Service implements Document {
     public _id: string | undefined;
-    public name: string | undefined;
-    public presets: string[] | undefined;
+    public name: string;
+    public presets: ServicePreset[];
+
+    constructor(data: (Partial<Service> & { name: string })) {
+        this.name = data.name;
+        this._id = data._id;
+        this.presets = data.presets || [];
+    }
 }
